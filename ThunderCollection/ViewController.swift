7541e89a424919cc199bc6633fc8ec1d783a4b85
Joel Trew <joel@3sidedcube.com>
2017-07-19 13:59:17 +0100

    Initial Commit

---
+++ ThunderCollection/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ThunderCollection
+//
+//  Created by Joel Trew on 19/07/2017.
+//  Copyright © 2017 3 Sided Cube. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
