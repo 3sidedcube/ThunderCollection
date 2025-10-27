1adb3056b2303ba0c970c5c0f73f12e58f6e893d
neil3sidedcube <neil@3sidedcube.com>
2025-10-27 11:11:36 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,6 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ThunderCollection",
-            type: .static,
             targets: [
                 "ThunderCollection"
             ]
