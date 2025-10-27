217f1482ad4de96a74c5dfa0c20935d772dd35da
neil3sidedcube <neil@3sidedcube.com>
2025-10-27 12:09:59 +0000

    Revert "Remove thundercollection header"
    This reverts commit 58fc2cb36d186d0acb22709368b6b2c43dda31be.

---
+++ ThunderCollection/ThunderCollection.h
@@ -0,0 +1,19 @@
+//
+//  ThunderCollection.h
+//  ThunderCollection
+//
+//  Created by Simon Mitchell on 31/07/2017.
+//  Copyright © 2017 3sidedcube. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ThunderCollection.
+FOUNDATION_EXPORT double ThunderCollectionVersionNumber;
+
+//! Project version string for ThunderCollection.
+FOUNDATION_EXPORT const unsigned char ThunderCollectionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ThunderCollection/PublicHeader.h>
+
+
